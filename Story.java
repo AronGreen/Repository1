9ce794ccae0ceddf9077e68ba1f8bcdbf493bd02
Marina Ionel <marinicaionel@gmail.com>
2018-09-14 13:18:23 +0200

    Story

---
+++ Story.java
@@ -0,0 +1,14 @@
+
+public class Story
+{
+   public static void main(String[] args)
+   {
+      double n;
+      n=1;
+      for (int m=0; m<=64; m++)
+      {
+         n=2*n;
+         System.out.println(n);
+      }
+   }
+}
